e6e982f4535e7b69c7dd96655c86862981179377
Busisile-Zilwa2601 <busisile.bo@gmail.com>
2018-05-10 02:14:28 +0200

    completed the index page, added few tests as well.

--- greet.js
+++ greet.js
@@ -1,26 +1,41 @@
-function Greet(languageSelected){
+function Greet(){
   var temp = {};
   var nameEntry = function(name){
-    if(temp[name] === undefined){
-      temp[name] = 0;
-    }
-    if(temp[name] == 1 ){
-      
-    }
-    else{
-      temp[name] += 1;
+    if(name != ''){
+      if(temp[name.toLowerCase()] === undefined){
+        temp[name.toLowerCase()] = 0;
+      }
+      if(temp[name.toLowerCase()] == 1 ){
+
+      }
+      else{
+        temp[name.toLowerCase()] += 1;
+      }
     }
   }
-  var theGreetings = function(name){
+  var theGreetings = function(languageSelected, name){
+    if(name != ''){
+        if(languageSelected === 'english'){
+          return 'Hello '+ name;
+        }
+        if(languageSelected === 'isixhosa'){
+          return 'Molo '+ name;
+        }
+        if(languageSelected === 'afrikaans'){
+          return 'Hallo '+ name;
+        }
+    }
+    else{
       if(languageSelected === 'english'){
-        return 'Hello '+ name;
+        return "Please enter your name on the text box, select the language of your choice then press the \'Greet Me\'";
       }
       if(languageSelected === 'isixhosa'){
-        return 'Molo '+ name;
+        return 'Nceda faka igama lakho kwibhokisi yombhalo, ukhethe ulwimi, uze ucinezele iqhosha lokubulisa. ';
       }
       if(languageSelected === 'afrikaans'){
-        return 'Hallo '+ name;
+        return 'Voer asseblief jou naam in die tekskassie in, kies die taal van jou keuse en druk die knoppie Gree my.';
       }
+    }
   }
   var sumObj = function(){
     var sum = 0;
--- greet.test.js
+++ greet.test.js
@@ -1,21 +1,42 @@
 describe("the Greet function", function(){
   it("It should return the \'Hello Busisile\' and \' Hello Nolupho\'", function(){
-    var greet = Greet('english');
+    var greet = Greet();
     greet.myName('Busisile');
     greet.myName('Nolupho');
     greet.myName('Nolupho');
     greet.myName('Busisile');
-    assert.deepEqual(greet.myGreetings('Busisile'), "Hello Busisile");
-    assert.deepEqual(greet.myGreetings('Nolupho'), "Hello Nolupho");
+    assert.deepEqual(greet.myGreetings('english', 'Busisile'), "Hello Busisile");
+    assert.deepEqual(greet.myGreetings('english', 'Nolupho'), "Hello Nolupho");
     assert.deepEqual(greet.mySum(), 2);
     console.log(greet);
   });
   it("It should return the \'Molo Busisile\' and \' Molo Nolupho\'", function(){
-    var greet = Greet('isixhosa');
+    var greet = Greet();
     greet.myName('Busisile');
     greet.myName('Nolupho');
-    assert.deepEqual(greet.myGreetings('Busisile'), "Molo Busisile");
-    assert.deepEqual(greet.myGreetings('Nolupho'), "Molo Nolupho");
-    assert.deepEqual(greet.mySum(), 2);
+    greet.myName('Odwa');
+    greet.myName('Odwa');
+    assert.deepEqual(greet.myGreetings('isixhosa', 'Busisile'), "Molo Busisile");
+    assert.deepEqual(greet.myGreetings('isixhosa', 'Nolupho' ), "Molo Nolupho");
+    assert.deepEqual(greet.mySum(), 3);
+  });
+  it("It should return the \'Hallo Busisile\' and \' Hallo Nolupho\'", function(){
+    var greet = Greet();
+    greet.myName('Busisile');
+    greet.myName('Nolupho');
+    greet.myName('Odwa');
+    greet.myName('odwa');
+    greet.myName('David');
+    assert.deepEqual(greet.myGreetings('afrikaans', 'Busisile'), "Hallo Busisile");
+    assert.deepEqual(greet.myGreetings('afrikaans', 'Nolupho' ), "Hallo Nolupho");
+    assert.deepEqual(greet.mySum(), 4);
+  });
+  it("If no name is entered, but a language is selected, after pressing \'Greet Me\' ,It should return a message on the language you selected. ", function(){
+    var greet = Greet();
+    greet.myName('');
+    assert.deepEqual(greet.myGreetings('afrikaans', ''), "Voer asseblief jou naam in die tekskassie in, kies die taal van jou keuse en druk die knoppie Gree my.");
+    assert.deepEqual(greet.myGreetings('english', '' ), "Please enter your name on the text box, select the language of your choice then press the \'Greet Me\'");
+    assert.deepEqual(greet.myGreetings('isixhosa', '' ), "Nceda faka igama lakho kwibhokisi yombhalo, ukhethe ulwimi, uze ucinezele iqhosha lokubulisa. ");
+    assert.deepEqual(greet.mySum(), 0);
   });
 })
